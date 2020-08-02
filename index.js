7d655bde7f168d8dcb3c2c2d4972c2c6e98fdebf
Roland Levy <rolandjlevy@gmail.com>
2020-08-02 22:46:22 +0000

    Fading in canvas

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const canvas = document.querySelector('#game-area');
   canvas.width = 400;
   canvas.height = 300;
+  canvas.style.opacity = 1;
   const ctx = canvas.getContext('2d');
 
   function clearCanvas() {
